e26c63099484a7c65e7e97c2af127ebf1b7e0eae
Szymon Kowalski <szkmk131@gmail.com>
2024-03-25 10:32:01 +0100

    check for new static root, add homepage

--- config/settings/base.py
+++ config/settings/base.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATIC_ROOT = str(BASE_DIR / "vercel/path0/staticfiles")
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
+from config.views import HomeView
+
 urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
     path('admin/', admin.site.urls),
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
