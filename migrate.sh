70b0eb41f4010fe631dfeb0d5d31fb84dc7982c5
Szymon Kowalski <szkmk131@gmail.com>
2024-03-25 11:08:23 +0100

    add test.py for testing

--- migrate.sh
+++ migrate.sh
@@ -2,3 +2,4 @@
 pip install -r requirements.txt
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
+python3.9 test.py
---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.getcwd())
+print(os.listdir())
+print('staticfiles' in os.listdir())
